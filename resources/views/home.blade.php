b92c18bf0ead31d0e3168c6cdc6cda418b5d0be4
Jarvin <Jarvin.Siegers@Student.GildeOpleidingen.nl>
2024-04-09 10:15:19 +0200

    style: add font family + size & add home page start

---
+++ resources/views/home.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.app')
+
+@section('content')
+    <h1>Welkom bij Autorijschool DriveSmart!</h1>
+    <p>Wij zijn een rijschool gebaseerd in Roermond en omstreken. Wat ons speciaal maakt, is dat wij ons richten op jongeren
+        met een fysieke beperking.</p>
+@endsection
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -52,9 +52,10 @@
 </head>
 
 <body>
-    <nav class="navbar navbar-expand-lg bg-body-tertiary">
+    <nav class="navbar navbar-expand-lg bg-primary">
         <div class="container-fluid">
-            <a class="navbar-brand" href="">DriveSmart</a>
+            <img src="{{ asset('images/logo.png') }}" width="5%" />
+            <a class="navbar-brand text-white" href="{{ route('home') }}">DriveSmart</a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
                 data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
                 aria-label="Toggle navigation">
@@ -63,15 +64,33 @@
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                     <li class="nav-item">
-                        <a class="nav-link" href="{{ route('home') }}">Home</a>
+                        <a class="nav-link text-white" href="{{ route('home') }}">Home</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="{{ route('instructors.index') }}">Instructeur</a>
+                        <a class="nav-link text-white" href="{{ route('instructors.index') }}">Instructeurs</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link text-white" href="{{ route('stripcards.index') }}">Strippenkaarten</a>
+                    </li>
+                    {{-- <li class="nav-item dropdown">
+                        <a class="nav-link text-white dropdown-toggle" role="button" data-bs-toggle="dropdown"
+                            href="#">Instructeur</a>
+                        <ul class="dropdown-menu">
+                            <li><a class="dropdown-item" href="{{ route('instructors.index') }}">Instructeur</a></li>
+                            <li><a class="dropdown-item" href="#">Strippenkaarten</a></li>
+                            <li>
+                                <hr class="dropdown-divider">
+                            </li>
+                            <li><a class="dropdown-item" href="#">Something else here</a></li>
+                        </ul>
+                    </li> --}}
+                    <li class="nav-item">
+                        <a class="nav-link text-white" href="">Contact</a>
                     </li>
                 </ul>
             </div>
             <a class="logout btn" href="{{ route('logout') }}"
-                onclick="event.preventDefault(); document.getElementById('logout-form').submit();">Logout</a>
+                onclick="event.preventDefault(); document.getElementById('logout-form').submit();">Uitloggen</a>
             <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
                 @csrf
             </form>
